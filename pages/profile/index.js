67527069f8401bc1691517ac25c5ca305a38caad
chris <chris@jingdigital.com>
2019-02-14 19:15:43 +0800

    saved

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -5,21 +5,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    loading: false,
+    errPage: false,
+    name: '名字',
+    crmData: {
+      show_title: 'CRM客户信息',
+      crm_info:[{
+        category_name:'ratingcustfield',
+        'custom_field_value': [{ label: 'Numminiprogram', value: '234'}]
+      }]
+    },
+    noteInfo: {
+      show_title: '销售笔记测试',
+      profile_note: {
+        // note: '笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记'
+        note: '笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记笔记'
+      }
+    },
+    tagsInfo: {
+      show_title:'我的标签（WorkWechat标签）测试',
+      tags: ['标签1', '标签1', '标签1', '标签1', '标签1', '标签1', '标签1']
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    wx.showLoading({
+      title: '加载中',
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  wx.hideLoading()
   },
 
   /**
